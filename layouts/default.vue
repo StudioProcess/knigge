2500aa7ec361d394974b82da37c4d3aaaa4083e3
llangit <hello@laurentlang.com>
2021-11-13 14:37:17 +0100

    mobile layout fixes / new about overlay close icon / do not trim white space only user messages

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,6 +10,25 @@ import smoothscroll from 'smoothscroll-polyfill'
 export default {
   mounted () {
     smoothscroll.polyfill()
+    this.addBodyClasses()
+  },
+  methods: {
+    addBodyClasses () {
+      const ua = window.navigator.userAgent
+      const mobileBrowser = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua)
+      const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i)
+      const webkit = !!ua.match(/WebKit/i)
+      const iOSSafari = iOS && webkit && !ua.match(/CriOS/i)
+      if (mobileBrowser) {
+        document.querySelector('body').classList.add('mobile-browser')
+      }
+      if (iOS) {
+        document.querySelector('body').classList.add('ios')
+      }
+      if (iOSSafari) {
+        document.querySelector('body').classList.add('ios-safari')
+      }
+    }
   }
 }
 </script>
